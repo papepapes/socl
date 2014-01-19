5070b4327862c4edd87310786ce8176413cc380d
papepapes <papepapes@gmail.com>
2014-01-19 06:37:00 +0000

    controller updates

--- web/index.php
+++ web/index.php
@@ -31,7 +31,8 @@ $app->error(function(\Exception $e, $code) use($app){
 
 $app->after(function(Request $request, Response $response){
 	$response->headers->set('Access-Control-Allow-Origin', '*');
-	$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');	
+	$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	$response->headers->set('Content-type', 'application/json');	
 });
 
 $app->run();
